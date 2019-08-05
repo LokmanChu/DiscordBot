c77e064807c199f055386efe6c0d01f374fdb03e
aqml15 <49743263+aqml15@users.noreply.github.com>
2019-08-05 10:37:26 -0700

    Add files via upload

---
+++ EventListener.java
@@ -0,0 +1,106 @@
+package com.github.aqml15.discordbot;
+
+import org.javacord.api.DiscordApi;
+import org.javacord.api.entity.user.User;
+
+import java.util.Arrays;
+
+public class EventListener {
+    DiscordApi api;
+    BlockedWords blockedWords = new BlockedWords();
+    ReportCommand reportCommand = new ReportCommand(api);
+    MembersListCommands listCommands = new MembersListCommands(api);
+    Spam spam = new Spam(api);
+
+    public EventListener(DiscordApi api) {
+        this.api = api;
+        setupMessageCreateListener();
+        listCommands.add(api.getCachedUsersByName("aqml15").iterator().next());
+    }
+
+    public void setupMessageCreateListener()
+    {
+        api.addMessageCreateListener(event -> {
+            String msg = event.getMessage().getContent();
+            User author = event.getMessageAuthor().asUser().get();
+            if (author.isBot()) {
+            	return;
+            }
+            //! CMD k
+            System.out.println(msg);
+            if (msg.substring(0, 1).equals("!")) {
+                String args[] = msg.substring(1, msg.length()).split(" ");
+                String cmd = args[0];
+                args = Arrays.copyOfRange(args, 1, args.length);
+
+                switch (cmd) {
+                    case "list":
+                         blockedWords.listWords(event.getChannel());
+                        break;
+                    case "add":
+                        blockedWords.tree.insert(args[0]);
+                        break;
+                    case "remove":
+                        blockedWords.tree.delete(args[0]);
+                        break;
+                    case "report":
+                        reportCommand.sendReportPM(event.getMessageAuthor().asUser().get());
+                        break;
+                    case "members":
+                    	listCommands.viewMembers(event.getChannel());
+                    	break;
+                    case "stats":
+                    	listCommands.viewStats(event.getChannel());
+                    	break;
+                    case "ban":
+                    	if (args.length < 2) return;
+                    	String name = args[0];
+                    	User user = api.getCachedUsersByName(name).iterator().next();
+                    	String reason = "";
+                    	for (int i = 1; i < args.length; i++) {
+                    		reason = reason + args[i] + " ";
+                    	}
+                    	listCommands.ban(user, reason);
+                    	break;
+                    case "striked?":
+                    	if (listCommands.list.contains(author.getId())) {
+                    		System.out.println(listCommands.list.getMember(author.getId()).isStriked());
+                    	}
+                    	break;
+                    default:
+                        event.getChannel().sendMessage("Command not found");
+                        break;
+                }
+            }
+            else if (event.isPrivateMessage() && reportCommand.pendingReporters.contains(author)) {
+            	System.out.println("report");
+                reportCommand.addPMReport(author,msg);
+            }
+            else {
+                blockedWords.checkMessage(event);
+                // spam
+                spam.spamCheck(event);
+                spam.spamStrikeTest(event);
+            }
+        });
+    }
+
+    public void onServerMemberJoin()
+    {
+    	api.addServerMemberJoinListener(event -> {
+    		listCommands.memberJoin(event.getUser());
+    	});
+		
+	}
+
+    public void onServerMemberLeave()
+    {
+    	api.addServerMemberLeaveListener(event -> {
+    		listCommands.memberLeave(event.getUser());
+    	});
+		
+	}
+	
+}
+
+
--- ReportCommand.java
+++ ReportCommand.java
@@ -3,6 +3,7 @@ package com.github.aqml15.discordbot;
 import java.awt.Color;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 import org.javacord.api.entity.channel.Channel;
@@ -11,87 +12,112 @@ import org.javacord.api.entity.message.MessageBuilder;
 import org.javacord.api.entity.message.MessageDecoration;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.javacord.api.entity.user.User;
-import org.javacord.api.event.message.MessageCreateEvent;
-import org.javacord.api.event.message.reaction.ReactionAddEvent;
-import org.javacord.api.listener.message.MessageCreateListener;
-import org.javacord.api.listener.message.reaction.ReactionAddListener;
+import org.javacord.api.DiscordApi;
+import org.javacord.api.entity.channel.ServerTextChannelBuilder;
+import org.javacord.api.entity.permission.Permissions;
+import org.javacord.api.entity.permission.PermissionsBuilder;
+import org.javacord.api.entity.permission.Role;
+import org.javacord.api.entity.server.Server;
 
-public class ReportCommand implements MessageCreateListener, ReactionAddListener {
-	
-	@Override
-	public void onMessageCreate(MessageCreateEvent event) {
-		// Upon receiving '!report' command, send DM for further instructions
-        if (event.getMessageContent().equalsIgnoreCase("!report")) {
-        	User author = event.getMessageAuthor().asUser().get();
-        	try {
-        		event.getChannel().sendMessage("Report Found!");
-				author.openPrivateChannel().get().sendMessage(
-						"To Report a User for breaking the rules, please reply to this DM with the following format:\n"
-						+ "[!!! <userid to be reported> : <'Message breaking the rule'>]\n"
-						+ "[Example: !!! JohnDoe : 'What the ****!']"
-						);
-			}
-        	catch(Exception e) {
-        		System.out.println("Report Failed!");
-        	}
+    public class ReportCommand {
+
+        DiscordApi api;
+        ArrayList<User> pendingReporters;
+
+        public ReportCommand(DiscordApi api) {
+            this.api = api;
+            pendingReporters = new ArrayList<User>(4);
         }
-        
-     // Check if receiving message is DM, if so paste message into a private channel for review later
-        if (event.isPrivateMessage()) {
-        	Channel channel = event.getApi().getChannelsByName("reports").iterator().next();
-        	System.out.println("channel: " + channel + " ... " + channel.toString());
-        	User author = event.getMessageAuthor().asUser().get();
-        	String message = event.getMessageContent();	
-        	DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-        	Date date = new Date();
-        	String fullDate = dateFormat.format(date);
-        	String offender = message.split("\\s+")[1];
-        	String reportMessage = "";
-        	String[] collection = message.split("\\s+");
-        	for (int i = 3; i < collection.length; i++) {
-        		reportMessage = reportMessage + " " + collection[i];
-        	}
-        	offender = offender.substring(0, offender.length()-1);
-  
-    		if (message.startsWith("!!!", 0)) {
-    			System.out.println("NAME: " + offender);
-    			System.out.println(date);
-    			if (event.getApi().getCachedUsersByName(offender).size() < 1) {
-    				System.out.println("Error! This USER does not exist!");
-    			}
-    			else {
-    				new MessageBuilder()
-    			    .append("Report!", MessageDecoration.BOLD, MessageDecoration.UNDERLINE)
-    			    .setEmbed(new EmbedBuilder()
-    			            .setTitle(fullDate)
-    			            .setDescription("Report!\n" + "Reporter: " + author + "\nOffender: " + event.getApi().getCachedUsersByName(offender).iterator().next() + "\nMessage: " + reportMessage)
-    			            .setColor(Color.ORANGE))
-    			    .send((TextChannel)channel);
-    			}
-    		}
-    		else {
-    			System.out.println("Incorrect Formatting, Please Try Again!");
-    		}
-    		System.out.println(author.getName() + " -> PM CONTENT -> " + message);
-        } 
-	}
 
-	@Override
-	public void onReactionAdd(ReactionAddEvent event) {
-		// TODO Auto-generated method stub
-		if (event.getChannel() == event.getApi().getChannelsByName("reports").iterator().next()) {
-    		if (event.getEmoji().equalsEmoji("ðŸ‘�")) {
-    			//TODO: Strike Offender
-    			event.getApi().getTextChannelsByName("reports").iterator().next().sendMessage("Striked Boi!");
-    			System.out.println("*****");
-    		}
-    		else if (event.getEmoji().equalsEmoji("ðŸ‘Ž")) {
-    			//TODO: Disregard Report
-    			event.getApi().getTextChannelsByName("reports").iterator().next().sendMessage("Disregarded Boi!");
-    			System.out.println("*****");
-    		}
-    	}
-		
-	}
-	
-}
+        public void setupReportChannel() {
+            Server server = api.getServers().iterator().next();
+
+            // Creates Mod role if none exists
+            Permissions modPermissions = new PermissionsBuilder().setAllAllowed().build();
+            Permissions noPermissions = new PermissionsBuilder().setAllDenied().build();
+            Role mod = null;
+
+            if (server.getRolesByName("Mod").size() == 0) {
+                mod = server.createRoleBuilder().setName("Mod").setPermissions(modPermissions).setColor(Color.CYAN).create().join();
+                mod.addUser(server.getOwner(), "Owner is Moderator");
+            }
+
+            // Creates Private Report Channel if none exists
+            if (server.getChannelsByName("reports").size() == 0) {
+                ServerTextChannelBuilder reports = server.createTextChannelBuilder();
+                System.out.println(server.getTextChannels());
+                reports.setName("reports");
+                reports.addPermissionOverwrite(mod, modPermissions);
+                reports.addPermissionOverwrite(server.getEveryoneRole(), noPermissions);
+                reports.create().join();
+                System.out.println("Report Channel successfully created...");
+            }
+        }
+
+        public void sendReportPM(User author){
+            try {
+                pendingReporters.add(author);
+                author.openPrivateChannel().get().sendMessage(
+                        "To Report a User for breaking the rules, please reply to this DM with the following format:\n"
+                                + "[??? <userid to be reported> : <'Message breaking the rule'>]\n"
+                                + "[Example: ??? JohnDoe : 'What the ****!']"
+                );
+            }
+            catch(Exception e) {
+                System.out.println("Report Failed!");
+            }
+        }
+
+        public void addPMReport(User author, String message) {
+            // Check if receiving message is DM, if so paste message into a private channel for review later
+        	System.out.println(api.getServerTextChannelsByName("reports").iterator().hasNext());
+        	System.out.println(api.getServerTextChannelsByName("reports").iterator().next());
+            Channel channel = api.getServerTextChannelsByName("reports").iterator().next();
+            System.out.println("channel: " + channel + " ... " + channel.toString());
+            DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+            Date date = new Date();
+            String fullDate = dateFormat.format(date);
+            String offender = message.split("\\s+")[1];
+            String reportMessage = "";
+            String[] collection = message.split("\\s+");
+            for (int i = 3; i < collection.length; i++) {
+                reportMessage = reportMessage + " " + collection[i];
+            }
+            offender = offender.substring(0, offender.length() - 1);
+
+            if (message.startsWith("???", 0)) {
+                System.out.println("NAME: " + offender);
+                System.out.println(date);
+                if (api.getCachedUsersByName(offender).size() < 1) {
+                    System.out.println("Error! This USER does not exist!");
+                } else {
+                    new MessageBuilder()
+                            .append("Report!", MessageDecoration.BOLD, MessageDecoration.UNDERLINE)
+                            .setEmbed(new EmbedBuilder()
+                                    .setTitle(fullDate)
+                                    .setDescription("Report!\n" + "Reporter: " + author + "\nOffender: " + api.getCachedUsersByName(offender).iterator().next() + "\nMessage: " + reportMessage)
+                                    .setColor(Color.ORANGE))
+                            .send((TextChannel) channel);
+                }
+            } else {
+                System.out.println("Incorrect Formatting, Please Try Again!");
+            }
+            System.out.println(author.getName() + " -> PM CONTENT -> " + message);
+
+        }
+
+
+        //TODO: Strike Offender
+        public void strikeOffender() {
+            api.getTextChannelsByName("reports").iterator().next().sendMessage("Striked Boi!");
+            System.out.println("*****");
+        }
+
+        //TODO: Disregard Report
+        public void disregardReport() {
+            api.getTextChannelsByName("reports").iterator().next().sendMessage("Disregarded Boi!");
+            System.out.println("*****");
+        }
+
+
+    }
